3320e5dee56eeac4f389d400f3b13e5892adceab
Arthur Song <arth3rs0ng@gmail.com>
2019-04-29 19:45:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,14 @@
 class Application
+  @@items = []
+  
   def call (env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     
     if req.path.match(/items/)
       item_name = req.patch.split("/items/").last
-      
+      if @@items.detect {|item| item.name == item_name}
+        
       
     else
       resp.write "Bad URL"
